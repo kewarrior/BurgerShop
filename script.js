dee7c4b9aceaaf09d25028468cc7e1779c043a9f
kewarrior <joao_pinho96@hotmail.com>
2024-07-25 20:48:12 +0100

    2

---
+++ script.js
@@ -0,0 +1,53 @@
+const menu = document.getElementById("menu")
+const cartBtn = document.getElementById("cart-btn")
+const cartModal = document.getElementById("cart-modal")
+const cartItemsContainer = document.getElementById("cart-items")
+const cartTotal = document.getElementById("cart-total")
+const checkoutBtn = document.getElementById("checkout-btn")
+const closeModalBtn = document.getElementById("close-modal-btn")
+const cartCounter = document.getElementById("cart-count")
+const addressInput =document.getElementById("address")
+const addressWarn =document.getElementById("address-warn")
+
+
+
+
+// Abrir o modal do Carrinho
+
+cartBtn.addEventListener("click", function (){
+    cartModal.style.display = "flex";
+})
+
+//Fechar o modal do Carrinho
+
+cartModal.addEventListener("click",function (event){
+    if (event.target ===cartModal){
+        cartModal.style.display = "none"
+    }
+})
+
+closeModalBtn.addEventListener("click",function (){
+    cartModal.style.display="none"
+})
+
+//Detecao do botao para adicionar no carrinho
+
+menu.addEventListener("click",function (event){
+
+    let parentButton = event.target.closest(".add-to-cart-btn")
+
+    if (parentButton){
+        const name = parentButton.getAttribute("data-name")
+        const price = parseFloat(parentButton.getAttribute("data-price"))
+
+    }
+})
+
+
+
+// Funcao para adicionar no Carrinho
+
+function addToCart(name,price){
+
+}
+
