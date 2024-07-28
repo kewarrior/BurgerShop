27d174a23773015951caa30b85213ab8285ecf20
kewarrior <joao_pinho96@hotmail.com>
2024-07-28 12:15:05 +0100

    Mudanca de Hora Abertura

--- script.js
+++ script.js
@@ -195,7 +195,7 @@ checkoutBtn.addEventListener("click",function (){
 function checkRestaurantOpen(){
     const data= new Date();
     const hora =data.getHours();
-    return hora >=18 && 22;
+    return hora >=12 && 22;
     //true = Restaurante Open
 }
 
